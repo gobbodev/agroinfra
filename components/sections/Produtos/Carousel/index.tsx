67f658cc478487cdc1246511f6cff04584b1ee7a
Gabriel <gabrielgobbo.contato@protonmail.com>
2024-11-16 17:21:04 -0300

    feat: responsivo do carousel de `Produtos`. note: carousel do shadcn ta meio bugado

--- components/sections/Produtos/Carousel/index.tsx
+++ components/sections/Produtos/Carousel/index.tsx
@@ -26,18 +26,18 @@ export function ProdutosCarousel() {
       ]}
     >
       <Container>
-        <CarouselContent className='z-20 -ml-6'>
+        <CarouselContent className='z-20 -ml-6 -mt-[4vh] mobile:-ml-4'>
           {dataProdutos.map((item, index) => (
             <CarouselItem
-              className='rounded-r-lg bg-preto-fraco ml-6 px-10 py-12 basis-[31%] mt-12 flex flex-col items-center text-branco'
+              className='rounded-r-xl bg-preto-fraco ml-6 px-10 py-12 basis-[31%] mt-12 flex flex-col items-center text-branco container:basis-[40%] laptop:basis-[50%] mobile:rounded-lg mobile:ml-4 mobile:basis-[60%] mobile_g:basis-[75%] mobile_m:basis-[91%] mobile_m:rounded-none mobile_sx:basis-[95%]'
               key={item.title + index}
             >
-              <div className='_margin-25b h-60 '>
+              <div className='_margin-25b h-60  mobile:_margin-13b'>
                 <Image
-                  className='min-h-[235px] rounded-md'
+                  className='min-h-[235px] max-w-[300px] rounded-md mobile:min-h-[210px] mobile:max-w-[250px]'
                   src={item.image.src}
                   alt={item.image.alt}
-                  width={400}
+                  width={300}
                   height={200}
                 />
               </div>
