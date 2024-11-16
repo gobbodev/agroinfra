86f80a1128ff43239f38af16df6817a831932055
Gabriel <gabrielgobbo.contato@protonmail.com>
2024-11-16 20:00:49 -0300

    feat: fade effect na imagem do banner pra evitar o flicker visual

--- components/sections/Banner/Background/index.tsx
+++ components/sections/Banner/Background/index.tsx
@@ -1,7 +1,10 @@
-'use server';
+'use client';
 import Image from 'next/image';
+import { useState } from 'react';
 
-export async function Background() {
+export function Background() {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
   return (
     <>
       <div className='_bag-image absolute h-screen z-0 mobile_g:h-[80vh]'>
@@ -9,9 +12,15 @@ export async function Background() {
           src={'/images/fazenda_bg.jpg'}
           alt='plantação de arroz em um final de tarde'
           fill
+          priority
+          onLoadingComplete={() => setImageLoaded(true)}
         />
       </div>
-      <div className='_overlay-full mobile_g:!h-[80vh] relative' />
+      <div
+        className={`_overlay-full mobile_g:!h-[80vh] relative transition-opacity duration-500
+        ${!imageLoaded && '!opacity-100'}
+        `}
+      />
     </>
   );
 }
